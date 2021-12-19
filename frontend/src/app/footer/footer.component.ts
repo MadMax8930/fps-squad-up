84c1aacadc25c802c0bf2deaff5d025c12c6b966
Max Surnin <madmax8930@gmail.com>
2021-12-19 23:17:19 +0100

    all done

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  showDiv = {
+    displayFooter : false
+  }
+  
+  faGamepad = faGamepad;
 
   constructor() { }
 
